0f9f10bfd63e0b8ab7041de2c0d65a736db8ac23
Samantha McLean <sem635@nau.edu>
2025-07-16 13:37:01 -0500

    commiting changes to notebooks BOD 7/16

--- Notebooks/prospectFunctionsSFH.py
+++ Notebooks/prospectFunctionsSFH.py
@@ -1,9 +1,9 @@
 # IMPORT STATEMENTS:
-
+import numpy as np
 
 ##################################################################################################################################################
 
-def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type=2, sfh_type=4, **extras):
+def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type=None, sfh_type=None, **extras):
     """Build a prospect.models.SpecModel object
 
     :param mass: (optional, default:None)
@@ -36,11 +36,9 @@ def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type
 
     # Change `isfree` so that all parameters that will be kept track of are identified 
     # in the `model` object as `free_params`
-    model_params['zred']['isfree'] = True
+    # model_params['zred']['isfree'] = True
 
-    if zred is None:
-        model_params['zred']['init'] = 0.0
-    else:
+    if zred is not None:
         model_params['zred']['init'] = zred
 
     if mass is not None:
@@ -49,24 +47,91 @@ def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type
     if logzsol is not None:
         model_params['logzsol']['init'] = logzsol
 
-    if tage is None:
-        model_params['tage']['init'] = 13.
-    else:
+    if tage is not None:
         model_params['tage']['init'] = tage
 
     if dust2 is not None:
         model_params['dust2']['init'] = dust2
-        
-        
-    model_params['imf_type']['init'] = imf_type
 
+    if imf_type is not None:
+        model_params['imf_type']['init'] = imf_type
 
-    model_params['sfh']['init'] = sfh_type
+    if sfh_type is not None:
+        model_params['sfh']['init'] = sfh_type
     
 
     model = SpecModel(model_params)
 
     return model
+
+##################################################################################################################################################
+
+def getObs(snr=10.0, ldist=10.0, **extras):
+    """Build a dictionary of observational data. The data should 
+    consist of photometry for a single galaxy.
+
+    :param snr:
+        The S/N to assign to the photometry.
+
+    :param ldist:
+        The luminosity distance to assume for translating absolute 
+        magnitudes into apparent magnitudes.
+
+    :returns obs:
+        A dictionary of observational data to use in the fit.
+    """
+    from prospect.utils.obsutils import fix_obs
+    import sedpy
+
+    obs = {}
+
+    filternames = ['jwst_f090w', 'jwst_f115w', 'jwst_f150w', 'jwst_f200w', 'jwst_f277w',
+                   'jwst_f356w', 'jwst_f410m', 'jwst_f444w', 'jwst_f770w', 'jwst_f1800w']
+    obs['filters'] = sedpy.observate.load_filters(filternames)
+
+    ################################################# FIX THE UNITS OF THIS DATA
+    M_AB = np.array([0.0148, 0.028, 0.0269, 0.322, 1.090, 1.334, 1.597, 1.725, 1.54, 1.96]) # These are currently in units of microJanskeys (fix)
+    ################################################# FIX THE UNITS OF THIS DATA
+
+    obs['maggies'] = 10**(-0.4*M_AB)
+    obs['maggies_unc'] = (1./snr) * obs['maggies']
+
+    obs['phot_wave'] = np.array([f.wave_effective for f in obs['filters']])
+
+    obs['wavelength'] = None
+    obs['spectrum'] = None
+    obs['unc'] = None
+    obs['mask'] = None
+
+    obs = fix_obs(obs)
+
+    return obs
+
+##################################################################################################################################################
+
+def getSps(zcontinuous=1, **extras):
+    """
+    :param zcontinuous:
+        A value of 1 ensures that we use interpolation between SSPs to
+        have a continuous metallicity parameter (`logzsol`)
+        See python-FSPS documentation for details
+    """
+
+    # imf_upper_limit = 300
+    # then imf_type = 0 (power law) (go through each of first 5 imf_type
+    from prospect.sources import CSPSpecBasis
+    sps = CSPSpecBasis(zcontinuous=zcontinuous)
+    return sps
+    
+##################################################################################################################################################
+
+
+
+
+
+
+
+
 
 ##################################################################################################################################################
 
