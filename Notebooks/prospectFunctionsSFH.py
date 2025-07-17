7e7cc4197fba8bfa83d5980ec46634fb18dac821
Samantha McLean <sem635@nau.edu>
2025-07-17 16:04:42 -0500

    commiting 7-17

---
+++ Notebooks/prospectFunctionsSFH.py
@@ -0,0 +1,136 @@
+# IMPORT STATEMENTS:
+import numpy as np
+
+##################################################################################################################################################
+
+def getModel(add_burst=False, add_trunc=False, **extras):
+
+    from prospect.models import SpecModel
+    from prospect.models.templates import TemplateLibrary
+
+    model_params = TemplateLibrary['parametric_sfh']
+
+    if add_burst:
+        model_params.update(TemplateLibrary['burst_sfh'])
+        model_params['const'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Solar Masses per year'}
+        if add_trunc:
+            model_params['sf_start'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
+            model_params['sf_trunc'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
+
+    for key in list(extras.keys()):
+        model_params[key]['init'] = extras[key]
+
+    model = SpecModel(model_params)
+    
+    return model
+
+##################################################################################################################################################
+
+def getObs(snr=10.0, ldist=10.0, **extras):
+    """Build a dictionary of observational data. The data should 
+    consist of photometry for a single galaxy.
+
+    :param snr:
+        The S/N to assign to the photometry.
+
+    :param ldist:
+        The luminosity distance to assume for translating absolute 
+        magnitudes into apparent magnitudes.
+
+    :returns obs:
+        A dictionary of observational data to use in the fit.
+    """
+    from prospect.utils.obsutils import fix_obs
+    import sedpy
+
+    obs = {}
+
+    filternames = ['jwst_f090w', 'jwst_f115w', 'jwst_f150w', 'jwst_f200w', 'jwst_f277w',
+                   'jwst_f356w', 'jwst_f410m', 'jwst_f444w', 'jwst_f770w', 'jwst_f1800w']
+    obs['filters'] = sedpy.observate.load_filters(filternames)
+
+    ################################################# FIX THE UNITS OF THIS DATA
+    M_AB = np.array([0.0148, 0.028, 0.0269, 0.322, 1.090, 1.334, 1.597, 1.725, 1.54, 1.96]) # These are currently in units of microJanskeys (fix)
+    ################################################# FIX THE UNITS OF THIS DATA
+
+    obs['maggies'] = 10**(-0.4*M_AB)
+    obs['maggies_unc'] = (1./snr) * obs['maggies']
+
+    obs['phot_wave'] = np.array([f.wave_effective for f in obs['filters']])
+
+    obs['wavelength'] = None
+    obs['spectrum'] = None
+    obs['unc'] = None
+    obs['mask'] = None
+
+    obs = fix_obs(obs)
+
+    return obs
+
+##################################################################################################################################################
+
+def getSps(zcontinuous=1, **extras):
+    """
+    :param zcontinuous:
+        A value of 1 ensures that we use interpolation between SSPs to
+        have a continuous metallicity parameter (`logzsol`)
+        See python-FSPS documentation for details
+    """
+
+    # imf_upper_limit = 300
+    # then imf_type = 0 (power law) (go through each of first 5 imf_type
+    from prospect.sources import CSPSpecBasis
+    sps = CSPSpecBasis(zcontinuous=zcontinuous)
+    return sps
+    
+##################################################################################################################################################
+
+
+
+
+
+
+
+
+
+##################################################################################################################################################
+
+
+
+
+
+
+
+
+
+##################################################################################################################################################
+
+
+
+
+
+
+
+
+
+##################################################################################################################################################
+
+
+
+
+
+
+
+
+
+##################################################################################################################################################
+
+
+
+
+
+
+
+
+
+##################################################################################################################################################
