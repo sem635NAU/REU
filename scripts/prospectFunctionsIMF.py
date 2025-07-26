df2129070cbc36e4a002435d3ce46de7b1263771
Samantha McLean <sem635@nau.edu>
2025-07-26 13:31:15 -0500

    emcee run with 50000

--- scripts/prospectFunctionsIMF.py
+++ scripts/prospectFunctionsIMF.py
@@ -1,66 +1,127 @@
+# IMPORT STATEMENTS:
+import numpy as np
+from matplotlib.pyplot import *
 
+##################################################################################################################################################
 
+def getObs(snr=10.0, ldist=10.0, **extras):
+    """Build a dictionary of observational data. The data should 
+    consist of photometry for a single galaxy.
 
-def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type=2, sfh_type=4, **extras):
-    """Build a prospect.models.SpecModel object
+    :param snr:
+        The S/N to assign to the photometry.
 
-    :param mass: (optional, default:None)
-        If given, produce spectra for this mass. Otherwise the mass will
-        be 1e10 solar masses.
+    :param ldist:
+        The luminosity distance to assume for translating absolute 
+        magnitudes into apparent magnitudes.
 
-    :param zred: (optional, default: None)
-        If given, produce spectra and observed frame photometry appropriate
-        for this redshift. Otherwise the redshift will be zero.
-
-    :param logzsol: (optional, default: None)
-        If given, fix the model metallicity (:math: `log(Z/Z_sun)`) to the given value.
-        Otherwise the metallicity will be set to -0.5.
-        
-    :param tage: (optional, default: None)
-        If given, produce spectra and model photometry appropriate for
-        this galactic age. Otherwise the age will be set to 13. Gyrs.
-
-    :param dust2: (optional, default: None)
-        If given, produce spectra that are appropriate for provided dust
-        attenuation. Otherwise attenuation will be set to 0.6.
-
-    :returns model:
-        An instance of prospect.models.SedModel
+    :returns obs:
+        A dictionary of observational data to use in the fit.
     """
-    from prospect.models import SpecModel
-    from prospect.models.templates import TemplateLibrary
+    from prospect.utils.obsutils import fix_obs
+    import sedpy
 
-    model_params = TemplateLibrary['ssp']
+    obs = {}
 
-    # Change `isfree` so that all parameters that will be kept track of are identified 
-    # in the `model` object as `free_params`
-    model_params['zred']['isfree'] = True
+    filternames = ['jwst_f090w', 'jwst_f115w', 'jwst_f150w', 'jwst_f200w', 'jwst_f277w',
+                   'jwst_f356w', 'jwst_f410m', 'jwst_f444w', 'jwst_f770w', 'jwst_f1800w']
+    obs['filters'] = sedpy.observate.load_filters(filternames)
 
-    if zred is None:
-        model_params['zred']['init'] = 0.0
-    else:
-        model_params['zred']['init'] = zred
+    ################################################# FIX THE UNITS OF THIS DATA
+    M_AB = np.array([0.0148, 0.028, 0.0269, 0.322, 1.090, 1.334, 1.597, 1.725, 1.54, 1.96]) # These are currently in units of microJanskeys (fix)
+    ################################################# FIX THE UNITS OF THIS DATA
 
-    if mass is not None:
-        model_params['mass']['init'] = mass
+    obs['maggies'] = 10**(-0.4*M_AB)
+    obs['maggies_unc'] = (1./snr) * obs['maggies']
 
-    if logzsol is not None:
-        model_params['logzsol']['init'] = logzsol
+    obs['phot_wave'] = np.array([f.wave_effective for f in obs['filters']])
 
-    if tage is None:
-        model_params['tage']['init'] = 13.
-    else:
-        model_params['tage']['init'] = tage
+    obs['wavelength'] = None
+    obs['spectrum'] = None
+    obs['unc'] = None
+    obs['mask'] = None
 
-    if dust2 is not None:
-        model_params['dust2']['init'] = dust2
+    obs = fix_obs(obs)
+
+    return obs
+
+##################################################################################################################################################
+
+def getSps(zcontinuous=1, **extras):
+    """
+    :param zcontinuous:
+        A value of 1 ensures that we use interpolation between SSPs to
+        have a continuous metallicity parameter (`logzsol`)
+        See python-FSPS documentation for details
+    """
+
+    # imf_upper_limit = 300
+    # then imf_type = 0 (power law) (go through each of first 5 imf_type
+    from prospect.sources import CSPSpecBasis
+    sps = CSPSpecBasis(zcontinuous=zcontinuous)
+    wspec = sps.wavelengths
+    return sps, wspec
+
+##################################################################################################################################################
+
+def get_fixed_params():
+    # IMFs = theta
+    params = {'zred': 3.548,
+              'mass': 1e8,
+              'logzsol': 0.0,
+              'dust2': 0.0,
+              'sfh': 0,       # SSP
+              'imf_type': 2,
+              'dust_type': 0,
+              'tage': 1.0,
+             }
+    
+    return params
+
+##################################################################################################################################################
+# Defining functions for plotting
+
+def plot_binned_imf(weights):
+
+    imf_bins = np.array([[0.08, 0.5], [0.5, 2.0], [2.0, 4.0], [4.0, 5.0], [5.0, 120.0]])
+
+    figure(figsize=(8,5))
+    
+    bin_labels = [f'{str(imf[0])}, {str(imf[1])}' for imf in imf_bins]
+    imf_labels = [f'imf_{_}' for _ in range(1,len(imf_bins)+1)]
+    
+    N = 2
+    
+    n = 0
+    x_arrs = np.empty([len(imf_bins), N])
+    y_arrs = np.empty([len(imf_bins), N])
+    for start, end, weight in zip(imf_bins[:,0],imf_bins[:,1], imf_weights):
+        x_arrs[n] = np.linspace(start, end, N)
+        y_arrs[n] = np.ones(N) * weight
+    
+        fill_between(x_arrs[n], y_arrs[n], 0, alpha=0.4, label=f'{imf_labels[n]}={weight:.2f}')
+        plot(x_arrs[n], y_arrs[n])
         
+        n+=1
+    
+    xscale('log')
+    ylim([0,1])
+    
+    xlabel(r'Mass [$M_{\odot}$]')
+    ylabel('dN/dm')
+    
+    legend()
 
-    model_params['imf_type']['init'] = imf_type
+    return
 
-    model = SpecModel(model_params)
 
-    return model
+
+
+
+
+##################################################################################################################################################
+# Defining functions for conversions
+
 
 
 ##################################################################################################################################################
@@ -68,7 +129,7 @@ def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type
 
 
 
-
+##################################################################################################################################################
 
 
 
@@ -78,7 +139,4 @@ def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type
 
 
 
-
-
-
-
+##################################################################################################################################################
--- scripts/prospectFunctionsSFH.py
+++ scripts/prospectFunctionsSFH.py
@@ -1,104 +1,6 @@
 # IMPORT STATEMENTS:
 import numpy as np
-
-##################################################################################################################################################
-
-# def getModel(mass=None, zred=None, logzsol=None, tage=None, dust2=None, imf_type=None, 
-#              sfh_type=None, sf_start=None, sf_trunc=None, fburst=None, fage_burst=None, **extras):
-#     """Build a prospect.models.SpecModel object
-
-#     :param mass: (optional, default:None)
-#         If given, produce spectra for this mass. Otherwise the mass will
-#         be 1e10 solar masses.
-
-#     :param zred: (optional, default: None)
-#         If given, produce spectra and observed frame photometry appropriate
-#         for this redshift. Otherwise the redshift will be zero.
-
-#     :param logzsol: (optional, default: None)
-#         If given, fix the model metallicity (:math: `log(Z/Z_sun)`) to the given value.
-#         Otherwise the metallicity will be set to -0.5.
-        
-#     :param tage: (optional, default: None)
-#         If given, produce spectra and model photometry appropriate for
-#         this galactic age. Otherwise the age will be set to 13. Gyrs.
-
-#     :param dust2: (optional, default: None)
-#         If given, produce spectra that are appropriate for provided dust
-#         attenuation. Otherwise attenuation will be set to 0.6.
-
-#     :returns model:
-#         An instance of prospect.models.SedModel
-#     """
-#     from prospect.models import SpecModel
-#     from prospect.models.templates import TemplateLibrary
-
-#     model_params = TemplateLibrary['parametric_sfh']
-#     model_params.update(TemplateLibrary['burst_sfh'])
-
-#     # Change `isfree` so that all parameters that will be kept track of are identified 
-#     # in the `model` object as `free_params`
-#     # model_params['zred']['isfree'] = True
-
-#     if sf_start is not None:
-#         model_params['sf_start'] = {'N': 1, 'isfree': False, 'init': sf_start, 'units': 'Gyrs'}
-#     if sf_trunc is not None:
-#         model_params['sf_trunc'] = {'N': 1, 'isfree': False, 'init': sf_trunc, 'units': 'Gyrs'}
-
-#     if zred is not None:
-#         model_params['zred']['init'] = zred
-
-#     if mass is not None:
-#         model_params['mass']['init'] = mass
-
-#     if logzsol is not None:
-#         model_params['logzsol']['init'] = logzsol
-
-#     if tage is not None:
-#         model_params['tage']['init'] = tage
-
-#     if dust2 is not None:
-#         model_params['dust2']['init'] = dust2
-
-#     if imf_type is not None:
-#         model_params['imf_type']['init'] = imf_type
-
-#     if sfh_type is not None:
-#         model_params['sfh']['init'] = sfh_type
-
-#     if add_burst:
-#         model_params.update(TemplateLibrary['burst_sfh'])
-
-#     else:
-#         for key in extras.keys()
-    
-
-#     model = SpecModel(model_params)
-
-#     return model
-
-##################################################################################################################################################
-
-def getModel(add_burst=False, add_trunc=False, **extras):
-
-    from prospect.models import SpecModel
-    from prospect.models.templates import TemplateLibrary
-
-    model_params = TemplateLibrary['parametric_sfh']
-
-    if add_burst:
-        model_params.update(TemplateLibrary['burst_sfh'])
-        model_params['const'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Solar Masses per year'}
-        if add_trunc:
-            model_params['sf_start'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
-            model_params['sf_trunc'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
-
-    for key in list(extras.keys()):
-        model_params[key]['init'] = extras[key]
-
-    model = SpecModel(model_params)
-    
-    return model
+from matplotlib.pyplot import *
 
 ##################################################################################################################################################
 
@@ -157,55 +59,223 @@ def getSps(zcontinuous=1, **extras):
     # then imf_type = 0 (power law) (go through each of first 5 imf_type
     from prospect.sources import CSPSpecBasis
     sps = CSPSpecBasis(zcontinuous=zcontinuous)
-    return sps
+    wspec = sps.wavelengths
+    return sps, wspec
+    
+##################################################################################################################################################
+# Defining all of the conversion functions to be used for priors
+
+# def sf_start_from_fstart(**sfh):
+#     pset = getNamespace(**sfh)
+#     return (pset.tage * pset.fstart)
+
+def sf_trunc_from_ftrunc(tage,ftrunc):
+    if ftrunc == 0.:
+        return 0.0
+    end_trunc = tage * ftrunc
+    return tage - end_trunc
+
+def tau_from_logtau(logtau):
+    return 10**logtau
     
 ##################################################################################################################################################
 
+# def getNamespace(**params):
+#     from argparse import Namespace
+#     pset = Namespace(**params)
+#     return pset
 
+##################################################################################################################################################
 
+def getBreakIndices(wspec):
+    
+    blue, red = 3620, 4000 # Indices used in De Graaff, 2025
+    
+    all_indices = np.zeros(len(wspec), dtype=bool)
+    blue_lower, blue_upper, red_lower, red_upper = False, False, False, False
+    
+    for i, wave in enumerate(wspec):
+        if wave > blue and blue_lower==False:
+            all_indices[i], blue_lower = True, True
+        elif wave > blue+100 and blue_upper==False:
+            all_indices[i-1], blue_upper = True, True
+        elif wave > red and red_lower==False:
+            all_indices[i], red_lower = True, True
+        elif wave > red+100 and red_upper==False:
+            all_indices[i-1], red_upper = True, True
+    
+    true_indices = [index for index, value in enumerate(all_indices) if value]
+    
+    return true_indices
 
+##################################################################################################################################################
 
+# Make sure that the parameter(s) for logzsol (and tage) are maximized as according to the original parameter fits from earlier work
+def get_fixed_params():
+    # tage, logtau, fburst, fage_burst, const, ftrunc = theta
+    params = {'zred': 3.548,
+              'mass': 1e8,
+              'logzsol': 0.0,
+              'dust2': 0.0,
+              'sfh': 1,
+              'imf_type': 2,
+              'dust_type': 0,
+              'tage': 1.0,
+              'logtau':-1.0,
+              'tau': 0.1,
+              'fburst': 0.0,
+              'fage_burst': 0.0,
+              'tburst': 0.0,
+              'const': 0.0,
+              'fstart':0.0,
+              'ftrunc':0.0,
+              'sf_start': 0.0,
+              'sf_trunc': 0.0}
+    return params
 
+##################################################################################################################################################
 
+def compute_break_strength(spec):
+    """This function will compute the balmer break strengh for prospecter spectra
+    by averaging the flux from 4000-4100 and from 3620-3720 Angstroms (rest-frame)
+    then dividing the redder average flux by the bluer (De Graaff, 2025).
+    """
+    blue_l, blue_u, red_l, red_u = 382, 492, 804, 914
+    return spec[red_l:red_u].mean() / spec[blue_l:blue_u].mean()
 
+##################################################################################################################################################
+
+def init_prospect_generation():
+    params = get_fixed_params()
+    sps, wspec = getSps()
+    obs = getObs()
+
+    return params, obs, sps, wspec
+
+##################################################################################################################################################
+
+def emcee_model(theta, params, obs, sps):
+    from prospect.models.transforms import tburst_from_fage
+
+    tage, logtau, fburst, fage_burst, const, ftrunc = theta
+    
+    params['tage'] = tage
+    params['logtau'] = logtau
+    params['tau'] = tau_from_logtau(logtau)
+    
+    # params['add_burst'] = True
+    params['fburst'] = fburst
+    params['fage_burst'] = fage_burst
+    params['tburst'] = tburst_from_fage(tage=tage, fage_burst=fage_burst)
+    
+    params['const'] = const
+    
+    # params['add_trunc'] = True
+    params['ftrunc'] = ftrunc
+    params['sf_trunc'] = sf_trunc_from_ftrunc(tage=tage,ftrunc=ftrunc)
+
+    spec = getSpec(obs, sps, **params)
+
+    Bpred = compute_break_strength(spec)
+
+    return Bpred
+
+##################################################################################################################################################
+
+def getSpec(obs, sps, add_burst=True, add_trunc=True, **params):
+    
+    from prospect.models import SpecModel
+    from prospect.models.templates import TemplateLibrary
+
+    model_params = TemplateLibrary['parametric_sfh']
+
+    if add_burst:
+        model_params.update(TemplateLibrary['burst_sfh'])
+        model_params['const'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Solar Masses per year'}
+        if add_trunc:
+            model_params['sf_start'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
+            model_params['sf_trunc'] = {'N': 1, 'isfree': False, 'init': 0.0, 'units': 'Gyrs'}
+
+    for key in list(model_params.keys()):
+        model_params[key]['init'] = params[key]
+        model_params[key]['isfree'] = False
+
+    model = SpecModel(model_params)
+
+    spec, _, _, = model.predict(model.theta, obs=obs, sps=sps)
+
+    return spec
+
+##################################################################################################################################################
+
+def log_prior(theta):
+    
+    tage, logtau, fburst, fage_burst, const, ftrunc = theta
+
+    # This is where all the priors will be set for each parameter in theta
+    tage_prior = 0.1 <= tage < 1.775 # Max age is the age of the universe at this redshift
+    logtau_prior = -1 <= logtau <= 2
+    fburst_prior = 0.0 <= fburst <= 1.0
+    fage_burst_prior = 0.0 <= fage_burst <= 1.0
+    const_prior = 0.0 <= const <= 1.0
+    ftrunc_prior = 0.0 <= ftrunc < 1.0
+
+    f_mass_prior = (const + fburst) <= 1.0
+
+    if tage_prior and logtau_prior and fburst_prior and fage_burst_prior and const_prior and ftrunc_prior and f_mass_prior:
+        return 0.0
+    return -np.inf #, [tage_prior, logtau_prior, fburst_prior, fage_burst_prior, const_prior, ftrunc_prior]
+
+##################################################################################################################################################
+
+def log_likelihood(theta, B, Berr, params, obs, sps):
+
+    # B, Berr = 6.9, 1.5
+    
+    Bpred = emcee_model(theta, params, obs, sps)
+    return -0.5 * (B - Bpred)**2 / Berr**2
+
+##################################################################################################################################################
+
+def log_probability(theta,B,Berr,params,obs,sps):
+    lp = log_prior(theta)
+
+    if not np.isfinite(lp):
+        return -np.inf
+
+    return lp + log_likelihood(theta,B,Berr,params,obs,sps)
+
+##################################################################################################################################################
+# Defining all the functions for useful plots
+
+def plot_spec(wspec, spec):
+    figure(figsize=(8,4))
+    plot(wspec[182:4713], spec[182:4713])
+    xlabel(r'Rest-frame wavelength [$\AA$]')
+    ylabel('Flux [maggies]')
+    return
+
+def plot_sfh(params):
+    from prospect.plotting.sfh import parametric_sfr
+    fig, ax = subplots()
+    times = np.linspace(0,params['tage'],10000)
+    sfrs = parametric_sfr(times=times,**params)
+    ax.plot(times,sfrs,label=f'tage={params['tage']:.2f} Gyrs')
+
+    ax.invert_xaxis()
+    ax.set_xlabel('lookback time')
+    ax.set_ylabel('sfr')
+    return
+
+def plot_imf(bins):
+
+    return
 
 ##################################################################################################################################################
 
 
 
-
-
-
-
-
-
 ##################################################################################################################################################
-
-
-
-
-
-
-
-
-
-##################################################################################################################################################
-
-
-
-
-
-
-
-
-
-##################################################################################################################################################
-
-
-
-
-
-
 
 
 
